8fc28d7e5d6f9e42cce420e97727891a8e8fd4a5
ViniciusLaureano <viniciusglaureano@gmail.com>
2025-02-25 20:55:09 -0300

    chore(saveGame): savegame finish function changed to saveGameInHistory

--- src/Validations.hs
+++ src/Validations.hs
@@ -12,18 +12,17 @@ validateStage3 :: [[(Int, Int)]] -> Bool
 validateStage3 matriz = playerPieces matriz 1 == 2 || playerPieces matriz 2 == 2
 
 
-
 finishGame :: [[(Int, Int)]] -> Int -> (Int, String, String) -> IO ()
 finishGame matriz totJogadas (jogador, nomeJogador1, nomeJogador2) = do
   let winner = if playerPieces matriz 1 > playerPieces matriz 2 then 1 else 2
 
-  saveGame winner nomeJogador1 nomeJogador2
+  saveGameInHistory winner nomeJogador1 nomeJogador2
 
 
 playerPieces :: [[(Int, Int)]] -> Int -> Int
 playerPieces matriz playerNumber = length [(x, y) | row <- matriz, (x, y) <- row, y == playerNumber]
 
 
-saveGame :: Int -> String -> String -> IO ()
-saveGame winner nomeJogador1 nomeJogador2 = do
+saveGameInHistory :: Int -> String -> String -> IO ()
+saveGameInHistory winner nomeJogador1 nomeJogador2 = do
   putStrLn "not implemented yet"
