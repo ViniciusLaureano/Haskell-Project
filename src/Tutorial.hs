5d4caca3cc1ea21e0abf9998516879adab1226a5
Théo <theo.evangelista.rodrigues@ccc.ufcg.edu.br>
2025-03-04 09:29:20 -0300

    atualizando tutorial

--- src/Tutorial.hs
+++ src/Tutorial.hs
@@ -4,5 +4,51 @@ import UI.HSCurses.Curses
 
 import WindowManipulation
 
-startTutorial :: Window -> IO ()
-startTutorial window = putStrLn "teste"
+import Board
+
+import GameComponents
+
+matriz = matrizDefault
+
+startTutorial :: Window -> IO()
+startTutorial window = do
+  boardGenerate (2, 4) matriz window
+  upMove matriz window
+
+upMove :: [[(Int, Int)]] -> Window -> IO()
+upMove matriz window = do
+  key <- getCh
+  case key of
+      KeyChar 'w' -> rightMove matriz window
+      _ -> upMove matriz window
+
+rightMove :: [[(Int, Int)]] -> Window -> IO()
+rightMove matriz window = do
+  boardGenerate (1, 4) matriz window
+  key <- getCh
+  case key of
+    KeyChar 'd' -> leftMove matriz window
+    _ -> rightMove matriz window
+
+leftMove :: [[(Int, Int)]] -> Window -> IO()
+leftMove matriz window = do
+  boardGenerate (1, 7) matriz window
+  key <- getCh
+  case key of
+    KeyChar 'a' -> downMove matriz window
+    _ -> leftMove matriz window
+ 
+downMove :: [[(Int, Int)]] -> Window -> IO()
+downMove matriz window = do
+  boardGenerate (1, 4) matriz window
+  key <- getCh
+  case key of
+    KeyChar 's' -> makeMove matriz window
+    _ -> downMove matriz window
+
+makeMove :: [[(Int, Int)]] -> Window -> IO()
+makeMove matriz window = do
+  boardGenerate (2, 4) matriz window
+  key <- getCh
+  case key of
+    KeyChar '\n' -> makeMove matriz window
